8f59461b49c4fc8499e44b22f86e4146ccf933fc
yarskii <yarigusev@gmail.com>
2021-07-31 00:35:43 +0300

    add lesson_6

--- geekshop/views.py
+++ geekshop/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
+from mainapp.views import get_basket
+
 
 def index(request):
-
+    title = 'магазин'
     context = {
+        'title': title,
+        'basket': get_basket(request.user),
         'slogan': 'супер предложения',
     }
     return render(request, 'geekshop/index.html', context)
 
 def contacts(request):
-    return render(request, 'geekshop/contact.html')
+    title = 'контакты'
+    context = {
+        'title': title,
+        'basket': get_basket(request.user),
+    }
+    return render(request, 'geekshop/contact.html', context)
