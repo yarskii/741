b5fad6966ac643965fbd87042fac62d3dc9cdfcb
yarskii <yarigusev@gmail.com>
2021-09-04 19:29:19 +0300

    add lesson_2.5

--- ordersapp/static/ordersapp/script.js
+++ ordersapp/static/ordersapp/script.js
@@ -47,7 +47,10 @@ window.onload = function () {
         var target_name= row[0].querySelector('input[type="number"]').name;
         orderitem_num = parseInt(target_name.replace('orderitems-', '').replace('-quantity', ''));
         delta_quantity = -quantity_arr[orderitem_num];
-        orderSummaryUpdate(price_arr[orderitem_num], delta_quantity);
+        quantity_arr[orderitem_num] = 0;
+        if (!isNaN(price_arr[orderitem_num]) && !isNaN(delta_quantity)) {
+            orderSummaryUpdate(price_arr[orderitem_num], delta_quantity);
+        }
     }
     $('.formset_row').formset({
         addText: 'добавить продукт',
@@ -69,5 +72,53 @@ window.onload = function () {
     $('.order_form select').change(function () {
         var target = event.target;
             console.log(target);
+    });
+
+    if (!order_total_quantity) {
+        orderSummaryRecalc()
+    }
+
+    function orderSummaryRecalc() {
+        order_total_quantity = 0;
+        order_total_price = 0;
+        total_forms = parseInt($('input[name=orderitems-TOTAL_FORMS]').val());
+
+        for (var i = 0; i < TOTAL_FORMS; i++) {
+            order_total_quantity += quantity_arr[i];
+            order_total_price += quantity_arr[i] * price_arr[i];
+        }
+        $('.order_total_quantity').html(order_total_quantity.toString());
+        $('.order_total_cost').html(Number(order_total_price.toFixed(2).toString()));
+    }
+
+    $('.order_form select').change(function () {
+        var target = event.target;
+        orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-product', ''));
+        var orderitem_product_pk = target.options[target.selectedIndex].value;
+
+        if (orderitem_product_pk) {
+            console.log($);
+
+            $.ajax({
+                url: "/order/product/" + orderitem_product_pk + "/price/",
+                success: function (data) {
+                    if (data.price) {
+                        price_arr[orderitem_num] = parseFloat(data.price);
+                        if (isNaN(quantity_arr[orderitem_num])) {
+                            quantity_arr[orderitem_num] = 0;
+                        }
+                        var price_html = '<span>' + data.price.toString().replace('.', ',') + '</span> руб';
+                        var current_tr = $('.order_form table').find('tr:eq(' + (orderitem_num + 1) + ')');
+
+                        current_tr.find('td:eq(2)').html(price_html);
+
+                        if (isNaN(current_tr.find('input[type="number"]').val())) {
+                            current_tr.find('input[type="number"]').val(0);
+                        }
+                        orderSummaryRecalc();
+                    }
+                },
+            });
+        }
     });
 };
--- ordersapp/views.py
+++ ordersapp/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db import transaction
 from django.db.models.signals import pre_save, pre_delete
 from django.dispatch import receiver
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
@@ -11,6 +11,7 @@ from django.forms import inlineformset_factory
 from basketapp.models import Basket
 from ordersapp.forms import OrderItemForm
 from ordersapp.models import Order, OrderItem
+from mainapp.models import Product
 
 
 class OrderList(LoginRequiredMixin, ListView):
@@ -145,3 +146,12 @@ def product_quantity_update_save(sender, update_fields, instance, **kwargs):
 def product_quantity_update_delete(sender, instance, **kwargs):
     instance.product.quantity += instance.quantity
     instance.product.save()
+
+
+def get_product_price(request, pk):
+    if request.is_ajax():
+        product = Product.objects.filter(pk=int(pk)).first()
+        if product:
+            return JsonResponse({'price': product.price})
+        else:
+            return JsonResponse({'price': 0})
